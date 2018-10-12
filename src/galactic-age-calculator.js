36924093fd63c4947afcb54a9f4e7c635ed43a36
Mel Yasuda <yasudamel@gmail.com>
2018-10-12 15:17:54 -0700

    add user interface

--- src/galactic-age-calculator.js
+++ src/galactic-age-calculator.js
@@ -1,10 +1,14 @@
 export class Age {
   constructor(year, gender) {
     this.year = year;
-    this.gender = gender
+    this.gender = gender;
+    this.lifeExpectancy = {
+      "M":80,
+      "F":90
+    };
   }
 
-  // current year - user's age
+  // current year - user's birth year
   earthAgeInDays(){
     var years = new Date();
     const EarthAge = years.getFullYear() - this.year;
@@ -32,62 +36,47 @@ export class Age {
   }
 
   leftYearsInMercury(){
-    const lifeExpectancy = {
-      "M":80,
-      "F":90
-    }
-    let leftYears = lifeExpectancy[this.gender] - this.ageInMercury();
+
+    let leftYears = this.lifeExpectancy[this.gender] - this.ageInMercury();
     if(leftYears === 0){
       return "You'll die this year";
     } else if (leftYears > 0) {
-      return "You have" + leftYears + "years left in Mercury."
+      return "You have" + leftYears + "years left in Mercury.";
     } else {
-      return "You have been dead for " + Math.abs(leftYears) + " years in Mercury."
+      return "You have been dead for " + Math.abs(leftYears) + " years in Mercury.";
     }
   }
 
   leftYearsInVenus(){
-    const lifeExpectancy = {
-      "M":80,
-      "F":90
-    }
-    let leftYears = lifeExpectancy[this.gender] - this.ageInVenus();
+    let leftYears = this.lifeExpectancy[this.gender] - this.ageInVenus();
     if(leftYears === 0){
       return "You'll die this year";
     } else if (leftYears > 0) {
-      return "You have " + leftYears + " years left in Venus."
+      return "You have " + leftYears + " years left in Venus.";
     } else {
-      return "You have been dead for " + Math.abs(leftYears) + " years in Venus."
+      return "You have been dead for " + Math.abs(leftYears) + " years in Venus.";
     }
   }
 
   leftYearsInMars(){
-    const lifeExpectancy = {
-      "M":80,
-      "F":90
-    }
-    let leftYears = lifeExpectancy[this.gender] - this.ageInMars();
+    let leftYears = this.lifeExpectancy[this.gender] - this.ageInMars();
     if(leftYears === 0){
       return "You'll die this year";
     } else if (leftYears > 0) {
-      return "You have " + leftYears + " years left in Mars."
+      return "You have " + leftYears + " years left in Mars.";
     } else {
-      return "You have been dead for " + Math.abs(leftYears) + " years in Mars."
+      return "You have been dead for " + Math.abs(leftYears) + " years in Mars.";
     }
   }
 
   leftYearsInJupiter(){
-    const lifeExpectancy = {
-      "M":80,
-      "F":90
-    }
-    let leftYears = lifeExpectancy[this.gender] - this.ageInJupiter();
+    let leftYears = this.lifeExpectancy[this.gender] - this.ageInJupiter();
     if(leftYears === 0){
       return "You'll die this year";
     } else if (leftYears > 0) {
-      return "You have " + leftYears + " years left in Jupiter."
+      return "You have " + leftYears + " years left in Jupiter.";
     } else {
-      return "You have been dead for " + Math.abs(leftYears) + " years in Jupiter."
+      return "You have been dead for " + Math.abs(leftYears) + " years in Jupiter.";
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
+
+import { Age } from '../src/galactic-age-calculator.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
+  $("#user-form").submit(function(event) {
+    $("year").val(' ');
+    event.preventDefault();
+    var userYear = $("#year").val();
+    var userGender = $("input:radio[name=gender]:checked").val();
+    var newAge = new Age(userYear, userGender);
+    $("#mercury").text(newAge.ageInMercury());
+    $("#venus").text(newAge.ageInVenus());
+    $("#mars").text(newAge.ageInMars());
+    $("#jupiter").text(newAge.ageInJupiter());
 
+    $("#mercury-expect").text(newAge.leftYearsInMercury());
+    $("#venus-expect").text(newAge.leftYearsInVenus());
+    $("#mars-expect").text(newAge.leftYearsInMars());
+    $("#jupiter-expect").text(newAge.leftYearsInJupiter());
+
+    $("#result").show();
+  });
 });
